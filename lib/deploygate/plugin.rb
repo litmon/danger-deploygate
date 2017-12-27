279a883f2b1d2d6ad3312452e3c94ac011128482
Fukuo Kadota <app.fk91@gmail.com>
2017-12-27 20:41:45 +0900

    Remove redundant variable

--- lib/deploygate/plugin.rb
+++ lib/deploygate/plugin.rb
@@ -17,25 +17,17 @@ module Danger
     #
     attr_writer :token
 
-    #
-    # Uploaded response
-    #
-    # @return [DeployGate::Response]
-    #
-    attr_reader :response
-
     def token
-      # default value can set from environment
       @token ||= ENV['DEPLOYGATE_API_TOKEN']
     end
 
     def upload(binary, filename, message = nil, distribution_name = nil)
       client = DeployGate::Client.new(user, token)
-      responses = client.upload(binary, filename, message, distribution_name)
+      response = client.upload(binary, filename, message, distribution_name)
 
-      app_name = responses['results']['name']
-      revision = "＃#{responses['results']['revision']}"
-      url = "https://deploygate.com#{responses['results']['path']}"
+      app_name = response['results']['name']
+      revision = "＃#{response['results']['revision']}"
+      url = "https://deploygate.com#{response['results']['path']}"
 
       message "DeployGate Uploaded #{app_name} #{revision}, see detail: #{url}"
     end
